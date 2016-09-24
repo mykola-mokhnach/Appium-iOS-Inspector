d596252065e6591440d885ede3e9602b20d7329e
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-09-24 14:02:34 +0200

    Add year and author to the license template

--- iOS Inspector_files/inspector.js
+++ iOS Inspector_files/inspector.js
@@ -11,7 +11,8 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  *
- * Copyright 2016 Nick at Wire Swiss Inc. Made the Inspector compatible with Appium iOS driver.
+ * Copyright 2016-present Mykola Mokhnach at Wire Swiss GmbH
+ * Made the Inspector compatible with Appium iOS driver.
  *
  */
 
